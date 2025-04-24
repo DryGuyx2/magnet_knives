4e8908d622ac73c48fdef3842f387c6d2836fddc
DryGuyx2 <130485254+DryGuyx2@users.noreply.github.com>
2025-04-24 15:45:32 +0200

    Switch out cursor placeholder

--- cursor/cursor.gd
+++ cursor/cursor.gd
@@ -2,7 +2,7 @@ extends Node2D
 class_name Cursor
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN);
 
 func _process(delta: float) -> void:
-	global_position = get_global_mouse_position()
+	global_position = get_global_mouse_position();
