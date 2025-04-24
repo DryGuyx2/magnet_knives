388344110abc1776d57b79ceebc4d7bd395a7e55
DryGuyx2 <130485254+DryGuyx2@users.noreply.github.com>
2025-04-24 15:15:54 +0200

    Add simple state machine for player

--- player/player.gd
+++ player/player.gd
@@ -1,17 +1,32 @@
 extends CharacterBody2D
 class_name Player
 
+enum State {
+	IDLE,
+	MOVING,
+}
+
 @export var move_speed: int = 10000;
+@export var initial_state: State = State.IDLE
 
 @onready var animation_component: AnimatedSprite2D = $AnimatedSprite2D
 
 var move_direction: Vector2 = Vector2.ZERO
+
+var current_state: State = initial_state
+
 func _physics_process(delta: float) -> void:
 	handle_input(delta)
+	update_state()
 	velocity = move_direction * move_speed * delta
 	handle_animations()
 	move_and_slide()
 
+func update_state() -> void:
+	if move_direction != Vector2.ZERO:
+		current_state = State.MOVING
+		return
+	current_state = State.MOVING
 
 func handle_input(delta: float) -> void:
 	move_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -22,8 +37,7 @@ func handle_animations():
 	else:
 		animation_component.flip_h = false
 	
-	if move_direction != Vector2.ZERO:
+	if current_state == State.MOVING:
 		animation_component.play("moving")
-		return
-	
-	animation_component.play("idle")
+	elif current_state == State.IDLE:
+		animation_component.play("idle")
