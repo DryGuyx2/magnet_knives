388ecae33b79630f504270fffb3c1fa89dd7e1eb
DryGuyx2 <130485254+DryGuyx2@users.noreply.github.com>
2025-05-02 13:17:05 +0200

    Add game timer

--- player/player_camera/player_camera.gd
+++ player/player_camera/player_camera.gd
@@ -4,8 +4,17 @@ class_name PlayerCamera
 @export var player: Player
 
 @onready var health_hud = $Health
+@onready var time = $Time
+var game_time = 0
 
 var health_hud_levels = ["empty", "half", "full"]
+
+func _process(delta: float) -> void:
+	game_time += delta
+	var seconds = int(game_time)
+	var minutes = (seconds/60)
+	seconds -= minutes * 60
+	time.text = "%s:%s" % [minutes, seconds]
 
 func _ready() -> void:
 	player.hurt.connect(_on_player_hurt)
