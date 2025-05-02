c39a91b2d686cc54849946e910570e7535ca5a68
DryGuyx2 <130485254+DryGuyx2@users.noreply.github.com>
2025-05-02 14:37:54 +0200

    Improve knife spawning system

--- knives/knife_spawner.gd
+++ knives/knife_spawner.gd
@@ -1,14 +1,20 @@
-extends Node2D
-class_name knife_spawner
+extends VisibleOnScreenNotifier2D
+class_name KnifeSpawner
 
-@onready var player = get_parent()
-@onready var main_scene = player.get_parent()
+var player: Player
+var main_scene: Main
 
 var KNIFE_SCENES = {
 	"simple": preload("res://knives/simple/simple_knife.tscn"),
 	"jpeg": preload("res://knives/jpeg/jpeg_knife.tscn"),
 	"walking": preload("res://knives/walking/walking_knife.tscn"),
 }
+
+func spawn(kind: String) -> void:
+	var knife = KNIFE_SCENES[kind].instantiate()
+	knife.player = player
+	knife.global_position = global_position
+	main_scene.add_child(knife)
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("test_1"):
---
+++ knives/knife_spawner_manager.gd
@@ -0,0 +1,30 @@
+extends Node2D
+class_name KnifeSpawnerManager
+
+@export var player: Player
+@export var main_scene: Main
+
+@onready var spawners: Array = get_children()
+var off_screen_spawners: Array = []
+
+var knife_types = ["jpeg", "simple", "walking"]
+
+func _ready() -> void:
+	for spawner in spawners:
+		spawner.player = player
+		spawner.main_scene = main_scene
+
+func _process(delta):
+	if Input.is_action_just_pressed("test_4"):
+		spawn_knife()
+
+func spawn_knife() -> void:
+	off_screen_spawners = []
+	for spawner in spawners:
+		if not spawner.is_on_screen():
+			off_screen_spawners.append(spawner)
+	
+	var chosen_spawner = off_screen_spawners.pick_random()
+	if not chosen_spawner:
+		return
+	chosen_spawner.spawn(knife_types.pick_random())
