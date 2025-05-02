15e4286c834843ef244cd9f2c803f17fec02c5c5
DryGuyx2 <130485254+DryGuyx2@users.noreply.github.com>
2025-05-02 02:07:58 +0200

    Remove emit of removed signal in start screen

--- screens/intro/intro_screen.gd
+++ screens/intro/intro_screen.gd
@@ -12,7 +12,6 @@ var hovered = false
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot") and hovered:
 		get_tree().paused = false
-		emit_signal("game_started")
 		queue_free()
 
 func _on_area_2d_mouse_entered() -> void:
