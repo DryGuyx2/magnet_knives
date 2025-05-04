0c193f2a740e22983122c94bc9bf6d280d3446b1
DryGuyx2 <130485254+DryGuyx2@users.noreply.github.com>
2025-05-04 18:32:29 +0200

    Fix hand ordering, fine tune knife speeds

--- player/player.gd
+++ player/player.gd
@@ -104,6 +104,7 @@ func handle_animations() -> void:
 		mouse_direction = "sideways"
 	
 	gun_animations.z_index = int(mouse_direction == "up") + 1
+	hands.z_index = int(mouse_direction != "up") + 2
 	
 	if current_state == State.MOVING:
 		player_animations.play("moving_%s" % mouse_direction)
